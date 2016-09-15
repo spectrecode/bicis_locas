9c1518093340e21cfd667a14f545dfcb77bd228c
spectrecode <mchristy.ccb@gmail.com>
2016-09-15 12:19:04 -0500

    añadiendo cambios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+    var nombre = document.getElementById("name").value;
+    var apellido = document.getElementById("lastname").value;
+    var correo = document.getElementById("input-email").value;
+    var contraseña = document.getElementById("input-password").value;
+
+        if(nombre == null || nombre.length == 0 || /^[A-Z][a-z]*/.test(nombre) == false) {
+            return alert('Ingresa tu Nombre y la primera letra con mayúscula');
+        }
+         else if (apellido == null || apellido.length == 0 || /^[a-z][a-z]*/.test(apellido) == false) {
+            return alert('Ingresa tu apellido y la primera letra con mayúscula');
+         }
+         else if (correo == null || correo.length == 0 || /^\s+$/.test(correo)) {
+           	return alert('Ingresa tu Correo');
+         }
+         else if (contraseña == null || contraseña.length == 0 || /^\s+$/.test(contraseña)) {
+           	return alert('Ingresa tu contraseña');
+         }
 }
